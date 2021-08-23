e45e8912cbf624e2752208167a53068dccb33b25
prem-12345 <premkpatil1997@gmail.com>
2021-08-23 13:56:23 +0530

    Welcome to Generics problems

---
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.generics;
+
+public class FindMaximum {
+
+    public static void main(String[] args) {
+
+        /**
+         * Welcome to find maximum problem using Generics
+         *
+         * @author prem
+         * @version 11.0
+         * @since 23/08/2021
+         */
+
+    }
+
+
+}
