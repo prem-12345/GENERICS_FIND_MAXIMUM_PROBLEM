349e63160868e1c1aec55a34b166fe590112786b
prem-12345 <premkpatil1997@gmail.com>
2021-08-23 18:51:51 +0530

    Added more then three parameters

--- src/com/bridgelabz/generics/FindMaximum.java
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -8,11 +8,13 @@ package com.bridgelabz.generics;
  * @since 23/08/2021
  */
 
+import java.util.Collection;
+
 /**
- * Refactor to create Generic class to take in three variables of generic type
+ * UC4 Extend the maximum method to take more then three parameters
  *
  * @author prem
- * @version 11.5
+ * @version 11.6
  * @since 23/08/2021
  */
 
@@ -21,34 +23,45 @@ public class FindMaximum<E extends Comparable<E>> {
     E firstValue;
     E secondValue;
     E thirdValue;
+    E[] extraParamaters;
 
-    public FindMaximum(E firstValue, E secondValue, E thirdValue) {
+    public FindMaximum(E firstValue, E secondValue, E thirdValue, E... extraParamaters) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
+        this.extraParamaters = extraParamaters;
     }
 
-    public static <E extends Comparable<E>> E getMax(E firstValue, E secondValue, E thirdValue) {
+    public static <E extends Comparable<E>> E getMax(E firstValue, E secondValue, E thirdValue, E... extraParamaters) {
         E max = firstValue;
         if (secondValue.compareTo(max) > 0)
             max = secondValue;
         if (thirdValue.compareTo(max) > 0)
             max = thirdValue;
+        if (extraParamaters.length != 0) {
+            for (E extraParamater : extraParamaters) {
+                if (extraParamater.compareTo(max) > 0) {
+                    max = extraParamater;
+                }
+            }
+        }
         return max;
     }
 
     public <E extends Comparable<E>> E getMax() {
-        E max = (E) getMax(firstValue, secondValue, thirdValue);
+        E max = (E) getMax(firstValue, secondValue, thirdValue, extraParamaters);
         return max;
     }
 
     public static void main(String[] args) {
+
         System.out.println("Maximum Value Of Integer:");
-        System.out.println(new FindMaximum(40, 10, 90).getMax());
+        System.out.println(new FindMaximum(40, 10, 90, 100).getMax());
         System.out.println("Maximum Value Of Flot:");
-        System.out.println(new FindMaximum(1.2f, 4.2f, 5.6f).getMax());
+        System.out.println(new FindMaximum(1.2f, 4.2f, 5.6f, 8.5f).getMax());
         System.out.println("Maximum Value Of String:");
-        System.out.println(new FindMaximum("apple", "banana", "peach").getMax());
+        System.out.println(new FindMaximum("apple", "banana", "peach", "grapes").getMax());
+
     }
 }
 
